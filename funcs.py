fb9dbb8daf78700865fe84dc5b3fc55addae7893
tychkov.vv <tyvl@yandex.ru>
2023-01-23 22:29:17 +0300

    Add def "subtract_two_numbers"

--- funcs.py
+++ funcs.py
@@ -1,3 +1,8 @@
 def add_two_numbers(number_1, number_2):
     """Складывает два числа"""
     return number_1 + number_2
+
+
+def subtract_two_numbers(number_1, number_2):
+    """Вычитает два числа"""
+    return number_1 - number_2
