7bd89f450330ee5fd6cc75d65bab04e48976a486
tychkov.vv <tyvl@yandex.ru>
2023-01-23 22:41:49 +0300

    Import "subtract_two_numbers" and execute

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from funcs import add_two_numbers
+from funcs import add_two_numbers, subtract_two_numbers
 
 
 def main():
     a = 10
     b = 5
     print(add_two_numbers(a, b))
+    print(subtract_two_numbers(a, b))
 
 
 if __name__ == "__main__":
